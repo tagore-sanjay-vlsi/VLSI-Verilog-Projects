ea2c7c2dfb521e3d332d9437cac688f1359bd2f7
tagore-sanjay-vlsi <tagoresanjay572@gmail.com>
2025-12-12 20:53:16 +0530

    Create counter_sync.v

---
+++ src/counter_sync.v
@@ -0,0 +1,14 @@
+module counter_sync (
+    input clk,
+    input reset,      // Active high synchronous reset
+    output reg [3:0] count
+);
+
+always @(posedge clk) begin
+    if (reset)
+        count <= 4'b0000;
+    else
+        count <= count + 1;
+end
+
+endmodule
