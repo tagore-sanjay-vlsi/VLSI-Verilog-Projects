50168b10958e602e514f3dfd6108128f21b9ff89
tagore-sanjay-vlsi <tagoresanjay572@gmail.com>
2025-12-12 20:56:34 +0530

    Create counter_async.v

---
+++ src/src/counter_async.v
@@ -0,0 +1,14 @@
+module counter_async (
+    input clk,
+    input reset,       // Active high asynchronous reset
+    output reg [3:0] count
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        count <= 4'b0000;
+    else
+        count <= count + 1;
+end
+
+endmodule
